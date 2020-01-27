79901a08fa4bf56b3e8a2907c926af98c1fbd636
Jadav Nirav <nirqvjadav90@gmail.com>
2020-01-27 17:45:24 +0530

    Update apb_tb.sv
    updated vresion

--- apb_tb.sv
+++ apb_tb.sv
@@ -14,7 +14,7 @@ wire PWRITE;
 wire [WIDTH-1:0] PWDATA;
 wire [WIDTH-1:0] PADDR;
 
-
+localparam CP = 10; 
 apb_bridge brige (
 		        .transfer(TRANSFER),	// peripheral transfer 
 			.pclk(PCLK),		// peripheral clock
@@ -32,23 +32,35 @@ apb_bridge brige (
 initial
 begin 
 	PCLK <= 0;
-	repeat(25) #5 PCLK <= !PCLK;
+	repeat(25) #(CP/2) PCLK <= !PCLK;
 end
 
 initial
 begin
-	TRANSFER <= 0;
-	PRESET <=0;
-	PREADY <=1;
-	#2
-	PRESET <= 1;
-	TRANSFER <=1;
+	TRANSFER <= 0;	PRESET <=0;	PREADY <=0;
+
+	#2	PRESET <= 1;	TRANSFER <=1;
+
+	#(CP-2)	#CP	TRANSFER <= 0;
+
+//	#CP #CP	#(CP+CP/2)	PREADY <=1;
+
+//	#CP	PREADY <=0;
+
+//	#CP	$finish;
+
+
 end
 
-//always @(posedge PENABLE)
-//	if(PENABLE)begin PREADY <= 1; #10  PREADY<= 0; end
-	
+
+always@(PENABLE)
+	begin
+	if(PENABLE)  PREADY <=1;
+	else if(!PENABLE) PREADY <= 0;
+	end
 
 
+
+always@(posedge PCLK)
+	$display("TRANSFER = %b,PCLK = %b,PENABLE = %b, PREADY = %b, PSELECT = %b",TRANSFER,PCLK,PENABLE,PREADY,PSELECT);
 endmodule 
-
